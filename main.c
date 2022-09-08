614c1d5a137fb743754400bf554257cf641cb6ee
Case Brophy <cabrophy@mtu.edu>
2022-09-08 11:22:56 -0400

    working

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include "./stb_image_write.h"
+#include "math.h"
 
 const int IMG_HEIGHT = 512;
 const int IMG_WIDTH = 512;
@@ -12,10 +13,8 @@ unsigned char *gradient;
 // blue in checkerboard = 0, 0, 255
 
 void makeCheckerBoard(int x, int y, unsigned char *image) {
-   // x = x+1;
     int row = x / 64; 
     int column = y / 64;
-    printf("%d\n", row);
     if ( (row % 2 == 0 && column % 2 == 0) || ( row % 2 == 1 && column % 2 == 1))  {
         image[0] = 255;
         image[1] = 0;
@@ -27,7 +26,39 @@ void makeCheckerBoard(int x, int y, unsigned char *image) {
     }
 }
 
-void makeGradient(int i, int j, unsigned char *image) {
+void makeGradient(int x, int y, unsigned char *image) {
+
+    if (x >= 2) {
+        x++;
+        if (x >= 255) {
+            x++;
+        }
+    } 
+    if (y >= 2) {
+        y++;
+        if (y >= 255) {
+            y++;
+        }
+    }
+
+    // when x is 0 the the red channel is off and when x 512, the red channel should be 255
+    float x_adj = x / 512.0;
+    // when y is 0 the greeen chanel is off and when why is 512 the green chanell should be 255
+    float y_adj = y / 512.0;
+
+    if (x == 255 || x == 256 || x == 257) {
+        printf("%f\n", x_adj);
+    }
+
+    float rval = floor(255.0 * x_adj);
+    float gval = floor(255.0 * y_adj);
+
+
+    //printf("%f\n", rval);
+    
+    image[0] = rval;    
+    image[1] = gval;    
+    image[2] = 128;    
 }
 
 int main(int argc, char **argv) {
@@ -50,7 +81,7 @@ int main(int argc, char **argv) {
     }
 
     stbi_write_png("checkerboard.png", IMG_WIDTH, IMG_HEIGHT, 3, checker, IMG_WIDTH*3);
-    //stbi_write_png("gradient.png", IMG_WIDTH, IMG_HEIGHT, 3, gradient, IMG_WIDTH*3);
+    stbi_write_png("gradient.png", IMG_WIDTH, IMG_HEIGHT, 3, gradient, IMG_WIDTH*3);
     free(checker);
     free(gradient);
 
